0ee3c0a25a619818cce90626c43704f4b3471c5a
StepDevCode <dstepurka101@gmail.com>
2024-11-03 15:46:20 +0600

    add categories block

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,36 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
+import { Api } from '@/services/api-client'
 
 import { Container } from '@/components'
 
 import HeroImage from '@/public/images/hero.png'
 
+interface Categories {
+	id: number
+	name: string
+}
+
 export default function Home() {
+	const [categories, setCategories] = React.useState<Categories[]>([])
+
+	React.useEffect(() => {
+		async function fetchCategories() {
+			try {
+				const data = await Api.categories.getCategories()
+				setCategories(data)
+
+				return data
+			} catch (e) {
+				console.log(e)
+			}
+		}
+
+		fetchCategories()
+	}, [])
+
 	return (
 		<>
 			<section
@@ -41,12 +66,31 @@ export default function Home() {
 								className='relative font-light text-2xl italic leading-[130%] text-white'
 								href={''}
 							>
-								Order shiping
+								Order shipping
 							</Link>
 						</div>
 					</div>
 				</Container>
 			</section>
+			<div>
+				<Container>
+					<ul className='flex flex-col items-center text-center py-40 max-w-[800px] w-full mx-auto'>
+						{categories?.map((category) => (
+							<li
+								className='border-[#cecece] border-b w-full py-10 first:border-t'
+								key={category.id}
+							>
+								<Link
+									className='font-garamond font-light text-6xl leading-[100%] text-center uppercase text-grey'
+									href={`/catalog/${category.id}`}
+								>
+									{category.name}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</Container>
+			</div>
 		</>
 	)
 }
